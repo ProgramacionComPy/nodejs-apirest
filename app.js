04149edc4ef75978d6e162a28f34ee803c07a263
Rodrigo Paszniuk <rodrigopaszniuk@gmail.com>
2016-06-23 14:21:02 -0400

    add client controller and routes

--- app.js
+++ app.js
@@ -15,14 +15,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());  
 app.use(methodOverride());
 
+// Import Models and Controllers
+var models     = require('./models/client')(app, mongoose);
+var ClientCtrl = require('./controllers/clients');
+
 var router = express.Router();
 
-// Index
+// Index - Route
 router.get('/', function(req, res) {  
    res.send("Hola Mundo - www.programacion.com.py");
 });
 
 app.use(router);
+
+// API routes
+var api = express.Router();
+
+api.route('/clients')  
+  .get(ClientCtrl.findAll)
+  .post(ClientCtrl.add);
+
+api.route('/clients/:id')  
+  .get(ClientCtrl.findById)
+  .put(ClientCtrl.update)
+  .delete(ClientCtrl.delete);
+
+app.use('/api', api);  
+
 
 // Start server
 app.listen(3000, function() {
---
+++ controllers/clients.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var Client  = mongoose.model('Client');
+
+//GET - Return all registers
+exports.findAll = function(req, res) {
+	Client.find(function(err, clients) {
+    if(err) res.send(500, err.message);
+    	console.log('GET /clients')
+		res.status(200).jsonp(clients);
+	});
+};
+
+//GET - Return a register with specified ID
+exports.findById = function(req, res) {
+	Client.findById(req.params.id, function(err, client) {
+    if(err) return res.send(500, err.message);
+    console.log('GET /clients/' + req.params.id);
+		res.status(200).jsonp(client);
+	});
+};
+
+//POST - Insert a new register
+exports.add = function(req, res) {
+	console.log('POST');
+	console.log(req.body);
+	var client = new Client({
+		name:    req.body.name,
+		email: 	  req.body.email,
+		genre:    req.body.genre
+	});
+	client.save(function(err, client) {
+		if(err) return res.send(500, err.message);
+    res.status(200).jsonp(client);
+	});
+};
+
+//PUT - Update a register already exists
+exports.update = function(req, res) {
+	Client.findById(req.params.id, function(err, client) {
+		client.name   = req.body.name;
+		client.email    = req.body.email;
+		client.genre = req.body.genre;
+		client.save(function(err) {
+			if(err) return res.send(500, err.message);
+      res.status(200).jsonp(client);
+		});
+	});
+};
+
+//DELETE - Delete a register with specified ID
+exports.delete = function(req, res) {
+	Client.findById(req.params.id, function(err, client) {
+		client.remove(function(err) {
+			if(err) return res.send(500, err.message);
+      res.status(200);
+		})
+	});
+};
